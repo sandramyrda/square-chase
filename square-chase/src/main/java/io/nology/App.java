d7f0cb49c77a57a80079468d09aed50903cf4220
Sandra Myrda <sandra@Sandras-MacBook-Pro.local>
2024-08-07 21:02:57 +1000

    features

--- square-chase/src/main/java/io/nology/App.java
+++ square-chase/src/main/java/io/nology/App.java
@@ -5,16 +5,24 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class App extends Application {
     double movePX = 20;
     Random random = new Random();
+    ArrayList<Square> blocks = new ArrayList<>();
+
+    Square block1 = new Square(1);
+
+    Square block2 = new Square(1);
+    Square block3 = new Square(1);
+
+    Square player = new Square(0);
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -23,36 +31,16 @@ public class App extends Application {
         // scene is where all the content is
         stage.setTitle("square chase");
 
-        int randomX = random.nextInt(300);
-        int randomY = random.nextInt(300);
-        // handle the situation when both random positions are the same - generate a new
-        // random
-        int randomX2 = random.nextInt(300);
-        int randomY2 = random.nextInt(300);
-
-        Rectangle rec1 = new Rectangle(randomX, randomY, 30, 30);
-        rec1.setFill(Color.rgb(89, 121, 88));
-        Rectangle rec2 = new Rectangle(randomX2, randomY2, 30, 30);
-
         Pane layout = new Pane();
         // StackPane centres all the children automatically
-        layout.getChildren().addAll(rec1, rec2);
+        layout.getChildren().addAll(player.element, block1.element, block2.element, block3.element);
 
         Scene scene = new Scene(layout, 300, 300);
         scene.setOnKeyPressed(event -> {
-            handleKeyPress(event, rec1, rec2);
+            handleKeyPress(event, player.element, block1.element);
         });
         stage.setScene(scene);
         stage.show();
-    }
-
-    private void checkCollision(Rectangle rectangle1, Rectangle rectangle2) {
-        if (rectangle1.getBoundsInParent().intersects(rectangle2.getBoundsInParent())) {
-            System.out.println("Collision!");
-            rectangle1.setFill(Color.RED);
-        } else {
-            rectangle1.setFill(Color.rgb(89, 121, 88));
-        }
     }
 
     private void handleKeyPress(KeyEvent event, Rectangle rectangle, Rectangle staticRec) {
@@ -61,16 +49,20 @@ public class App extends Application {
 
         if (event.getCode() == KeyCode.UP) {
             rectangle.setY(y - movePX);
-            checkCollision(rectangle, staticRec);
+            player.doesItCollide(block1);
+            player.handleMove();
         } else if (event.getCode() == KeyCode.DOWN) {
             rectangle.setY(y + movePX);
-            checkCollision(rectangle, staticRec);
+            player.doesItCollide(block1);
+            player.handleMove();
         } else if (event.getCode() == KeyCode.LEFT) {
             rectangle.setX(x - movePX);
-            checkCollision(rectangle, staticRec);
+            player.doesItCollide(block1);
+            player.handleMove();
         } else if (event.getCode() == KeyCode.RIGHT) {
             rectangle.setX(x + movePX);
-            checkCollision(rectangle, staticRec);
+            player.doesItCollide(block1);
+            player.handleMove();
         }
     }
 
---
+++ square-chase/src/main/java/io/nology/Square.java
@@ -0,0 +1,64 @@
+package io.nology;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Square {
+    // type - player(0) or collision block(1)
+    Rectangle element;
+    int type;
+    Random random = new Random();
+    static ArrayList<Square> allBlocks = new ArrayList<>();
+
+    public Square(int type) {
+        this.type = type;
+        this.element = new Rectangle(30, 30);
+        generatePosition();
+        if (type == 0) {
+            this.element.setFill(Color.rgb(89, 121, 88));
+        } else {
+            allBlocks.add(this);
+        }
+    }
+
+    public boolean doesItCollide(Square sq) {
+        if (this.element.getBoundsInParent().intersects(sq.element.getBoundsInParent())) {
+            System.out.println("Collision!");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void generatePosition() {
+        boolean positionValid = false;
+        while (!positionValid) {
+            int randomX = random.nextInt(270);
+            int randomY = random.nextInt(270);
+            this.element.setX(randomX);
+            this.element.setY(randomY);
+            positionValid = true;
+            for (Square square : allBlocks) {
+                if (this != square && this.doesItCollide(square)) {
+                    positionValid = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    // method for the player
+    public void handleMove() {
+
+        for (Square sq : allBlocks) {
+            if (this.doesItCollide(sq)) {
+                // sq.element.setFill(Color.rgb(255, 0, 0));
+                sq.generatePosition();
+            }
+        }
+    }
+
+}
